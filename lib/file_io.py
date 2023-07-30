59dd2505858b6dbfec0f8fa95b9c6aa3a062c433
Peter Njiru <peternjirum@gmail.com>
2023-07-29 20:42:31 -0400

    completes fileio lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,12 @@
-def write_file(file_name, file_content):
-    pass
+def write_file(file_name='test_file', file_content="This is a test content."):
+    log_file = open(f'{file_name}.txt', mode='w', encoding='utf-8')
+    log_file.write(file_content)
 
-def append_file(file_name, append_content):
-    pass
+def append_file(file_name="test_file", append_content="\nAppended content."):
+    log_file = open(f'{file_name}.txt', mode='a', encoding='utf-8')
+    log_file.write(append_content)
 
-def read_file(file_name):
-    pass
+def read_file(file_name="test_file"):
+    # write_file(file_name)
+    log_file = open(f'{file_name}.txt', mode='r', encoding='utf-8')
+    return log_file.read()
